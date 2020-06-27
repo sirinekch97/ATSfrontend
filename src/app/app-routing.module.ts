2b472958a39c0ca8c9246f1c47fe88a86ce36140
sirine kchaou <sirinekch97@gmail.com>
2020-06-27 19:24:15 +0100

    product final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import{HomeComponent} from './home/home.component'
 
 
-const routes: Routes = [{path:'',component:HomeComponent,children:[{path:'product',loadChildren:'./home/produitfina/produitfinal.module#ProduitfinalModule'}]}];
+const routes: Routes = [{path:'',component:HomeComponent,children:[{path:'product',loadChildren:'./home/produitfinal/produitfinal.module#ProduitfinalModule'}]}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
